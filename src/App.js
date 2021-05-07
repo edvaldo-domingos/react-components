9b995d5d6008ae8666b35772c14c04661d814801
edvlado <edvlado@edvaldo-domingos-pc.home>
2021-05-07 18:49:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {InputGroup} from './components/compound-components/InputGroup.component'
+import {AiOutlineSearch} from 'react-icons/ai';
 
 function App() {
+  const [value, setValue] = React.useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="group">
+        <InputGroup 
+          id="input-1"
+          value={value}
+          onChange={event => setValue(event.target.value)}>
+          <InputGroup.Input />
+          <InputGroup.Label className="label"> Amount</InputGroup.Label>
+        </InputGroup>
+      </div>
+      <div className="group">
+        <InputGroup
+          id="input-1"
+          value={value}
+          onChange={event => setValue(event.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <InputGroup.Label className="bg-dark">Search</InputGroup.Label>
+          <InputGroup.Input />
+          <InputGroup.Item>
+            <AiOutlineSearch />
+          </InputGroup.Item>
+        </InputGroup>
+      </div>
+     
     </div>
   );
 }
---
+++ src/components/compound-components/InputGroup.component.js
@@ -0,0 +1,66 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./InputGroup.component.module.css";
+
+const InputGroupContext = React.createContext();
+
+const useInputGroupContext = () => {
+  const context = React.useContext(InputGroupContext);
+  if (!context) {
+    throw new Error(
+      `InputGroup compound components cannot be rendered outside the InputGroup component`
+    );
+  }
+  return context;
+};
+
+const Label = ({ className, ...props }) => {
+  const context = React.useContext(InputGroupContext);
+
+  return (
+    <label
+      htmlFor={context.id}
+      className={classNames(
+        styles.label,
+        styles.inputGroupItem,
+        styles.label,
+        className
+      )}
+    >
+      {props.children}
+    </label>
+  );
+};
+
+const Item = ({ className, ...props }) => {
+  return (
+    <div className={classNames(styles.inputGroupItem, className)}>
+      {props.children}
+    </div>
+  );
+};
+
+const Input = ({ className, ...props }) => {
+  const { id, value, onChange } = useInputGroupContext();
+
+  return (
+    <input
+      className={classNames(styles.inputGroupItem, styles.input)}
+      id={id}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export const InputGroup = props => {
+  return (
+    <InputGroupContext.Provider value={props}>
+      <div className={styles.container}>{props.children}</div>
+    </InputGroupContext.Provider>
+  );
+};
+
+InputGroup.Label = Label;
+InputGroup.Item = Item;
+InputGroup.Input = Input;
